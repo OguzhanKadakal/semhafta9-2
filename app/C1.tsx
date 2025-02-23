752ff1c8a213070ffd9d84e5132f8050dbceeccb
OguzhanKadakal <76901135+OguzhanKadakal@users.noreply.github.com>
2025-02-23 20:33:44 +0300

    Refactor Home component and add new components C1, C2, and C3

---
+++ app/C1.tsx
@@ -0,0 +1,11 @@
+import C2 from "./C2";
+
+export default function C1() {
+
+    return (
+        <div className="border border-gray- p-4 mt-4 rounded">
+            <h1>Component 1</h1>
+            <C2 />
+        </div>
+    )
+}
---
+++ app/C3.tsx
@@ -0,0 +1,9 @@
+export default function C3() {
+
+    return (
+        <div className="border border-gray-800 p-4 mt-4 rounded">
+            <h1>Component 3</h1>
+            <p>Son komponent</p>
+        </div>
+    )
+}
